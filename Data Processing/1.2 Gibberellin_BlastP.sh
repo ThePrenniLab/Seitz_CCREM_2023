02699cca66113cd06eee8ec880c800f30be599c7
Valerie Seitz <74557888+lindsval@users.noreply.github.com>
2023-11-16 10:32:44 -0700

    Update 1.2 Gibberellin_BlastP.sh

--- Data Processing/1.2 Gibberellin_BlastP.sh
+++ Data Processing/1.2 Gibberellin_BlastP.sh
@@ -3,6 +3,8 @@
 
 #!/bin/bash
 
+#used GA_genes.faa as query --> see 1.3 GA_genes.faa for that file
+
 #blastp
 makeblastdb -in /home/projects/Agribiome/RootExudate_Prenni/MetaG/MAG_database/dRep_v2.6.2_326/dereplicated_genomes/DRAM_1.4.4_05222023/genes.faa -dbtype prot -title gibberellin
 blastp -query GA_genes.faa -db /home/projects/Agribiome/RootExudate_Prenni/MetaG/MAG_database/dRep_v2.6.2_326/dereplicated_genomes/DRAM_1.4.4_05222023/genes.faa -out gibberellin_genes.txt -outfmt 6
